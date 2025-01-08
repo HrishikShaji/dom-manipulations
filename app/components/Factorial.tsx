b17477ce283c67512bb5d961675e467008e7b71a
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-01-08 22:24:05 +0530

    recursives added

--- app/components/Factorial.tsx
+++ app/components/Factorial.tsx
@@ -4,11 +4,35 @@ export default function Factorial() {
   const [input, setInput] = useState<number>(0)
   const [output, setOutput] = useState<null | number>(null)
 
-  function run() {
+  function factorial(n: number) {
     let num = 1;
-    for (let i = input; i > 0; i--) {
+    for (let i = n; i > 0; i--) {
       num = num * i
     }
+    return num;
+  }
+
+  function recursiveFactorial(n: number) {
+
+    function multiply(i: number, acc: number) {
+      if (i <= 0) {
+        return acc;
+      }
+      return multiply(i - 1, i * acc)
+    }
+
+    return multiply(n, 1)
+
+  }
+
+
+  function run() {
+    const num = factorial(input)
+    setOutput(num)
+  }
+
+  function runRecursive() {
+    const num = recursiveFactorial(input)
     setOutput(num)
   }
 
@@ -21,6 +45,7 @@ export default function Factorial() {
       type="number"
     />
     <button onClick={run} className="p-2 rounded-md text-white bg-green-500">Run</button>
+    <button onClick={runRecursive} className="p-2 rounded-md text-white bg-green-500">Run Recursive</button>
     <pre>
       <code>{output}</code>
     </pre>
--- app/components/Fibonacci.tsx
+++ app/components/Fibonacci.tsx
@@ -4,11 +4,35 @@ export default function Fibonacci() {
   const [output, setOutput] = useState<string>("")
   const [input, setInput] = useState<number>(0)
 
-  function run() {
+  function fibonacci(n: number) {
     let arr: number[] = [0, 1]
     for (let i = 2; i <= input; i++) {
       arr[i] = arr[i - 1] + arr[i - 2]
     }
+    return arr
+  }
+
+  function run() {
+    const arr = fibonacci(input)
+    setOutput(JSON.stringify(arr))
+  }
+
+  function recursiveFibonacci(n: number) {
+
+    let arr: number[] = [0, 1]
+    function addNum(i: number) {
+      arr[i] = arr[i - 1] + arr[i - 2]
+      if (i < n) {
+        addNum(i + 1)
+      }
+    }
+    addNum(2)
+    return arr
+
+  }
+
+  function runRecursive() {
+    const arr = recursiveFibonacci(input)
     setOutput(JSON.stringify(arr))
   }
 
@@ -22,7 +46,8 @@ export default function Fibonacci() {
         value={input}
         type="number"
       />
-      <button onClick={() => run()}>Run</button>
+      <button className="bg-green-500 text-white p-2 rounded-md" onClick={run}>Run</button>
+      <button className="bg-green-500 text-white p-2 rounded-md" onClick={runRecursive}>Run Recursive</button>
       <pre>
         <code>{output}</code>
       </pre>
