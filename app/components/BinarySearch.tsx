c9e2c917233fe912f91d3072ff9456bca3536d52
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-01-09 12:57:42 +0530

    added recursive for search

--- app/components/BinarySearch.tsx
+++ app/components/BinarySearch.tsx
@@ -28,9 +28,35 @@ export default function BinarySearch() {
 
   }
 
+  function recursiveBinarySearch(num: number, arr: number[]) {
+    return search(num, arr, 0, arr.length - 1)
+  }
+
+  function search(num: number, arr: number[], startIndex: number, endIndex: number) {
+    if (startIndex > endIndex) {
+      return -1
+    }
+    const middleIndex = Math.floor((startIndex + endIndex) / 2)
+
+    if (num === arr[middleIndex]) {
+      return middleIndex
+    }
+
+    if (num > arr[middleIndex]) {
+      return search(num, arr, middleIndex + 1, endIndex)
+    } else {
+      return search(num, arr, startIndex, middleIndex - 1)
+    }
+  }
+
   function run() {
     const index = binarySearch(input, array)
     console.log("index is", index)
+  }
+
+  function runRecursive() {
+    const index = recursiveBinarySearch(input, array)
+    console.log("index is recursive", index)
   }
 
   return <div className="flex flex-col gap-2">
@@ -42,6 +68,7 @@ export default function BinarySearch() {
       type="number"
     />
     <button onClick={run} className="p-2 rounded-md text-white bg-green-500">Run</button>
+    <button onClick={runRecursive} className="p-2 rounded-md text-white bg-green-500">Run Recursive</button>
     <pre>
       <code>{output}</code>
     </pre>
